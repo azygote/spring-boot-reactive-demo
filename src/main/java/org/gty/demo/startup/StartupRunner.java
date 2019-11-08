c8a00b5b209bd7611bd4dd5f1df285ddac363538
Tianyu Ge <wierzard@hotmail.com>
2019-11-08 15:47:36 -0500

    Introduced Java 13 features

--- src/main/java/org/gty/demo/startup/StartupRunner.java
+++ src/main/java/org/gty/demo/startup/StartupRunner.java
@@ -1,5 +1,6 @@
 package org.gty.demo.startup;
 
+import org.gty.demo.constant.SystemConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
@@ -8,6 +9,7 @@ import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Nonnull;
+import java.time.ZonedDateTime;
 import java.util.Objects;
 
 @Service
@@ -19,16 +21,44 @@ public class StartupRunner {
 
     public StartupRunner(@Nonnull ReactiveRedisTemplate<Object, Object> reactiveRedisTemplate) {
         this.reactiveRedisTemplate =
-                Objects.requireNonNull(reactiveRedisTemplate, "redisTemplate must not be null");
+            Objects.requireNonNull(reactiveRedisTemplate, "redisTemplate must not be null");
     }
 
     private void flushRedis() {
         reactiveRedisTemplate.
-                execute(connection -> connection
-                        .serverCommands()
-                        .flushDb())
-                .doOnComplete(() -> log.info("Successfully flushed redis."))
-                .subscribe();
+            execute(connection -> connection
+                .serverCommands()
+                .flushDb())
+            .doOnComplete(() -> log.info("Successfully flushed redis."))
+            .doOnComplete(this::newFeature)
+            .subscribe();
+    }
+
+    private void newFeature() {
+        var logInfo = """
+        This is log information.
+        Breaks in multiple lines.
+        """;
+
+        log.info(logInfo);
+
+        var dateTime = ZonedDateTime.now(SystemConstants.defaultTimeZone);
+        var monthInfo = "This is " + switch (dateTime.getMonth()) {
+            case JANUARY -> "JAN";
+            case FEBRUARY -> "FEB";
+            case MARCH -> "MAR";
+            case APRIL -> "APR";
+            case MAY -> "MAY";
+            case JUNE -> "JUN";
+            case JULY -> "JUL";
+            case AUGUST -> "AUG";
+            case SEPTEMBER -> "SEPT";
+            case OCTOBER -> "OCT";
+            case NOVEMBER -> "NOV";
+            default -> "DEC";
+        };
+
+        log.info(monthInfo);
     }
 
     @EventListener
