933db326c37baafd55ab11405e7e3b387a7dd669
Tianyu Ge <wierzard@hotmail.com>
2020-06-14 14:48:41 -0700

    Added configurable properties for Feign

--- src/main/java/org/gty/demo/github/service/ReactiveGithubService.java
+++ src/main/java/org/gty/demo/github/service/ReactiveGithubService.java
@@ -1,19 +1,13 @@
 package org.gty.demo.github.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import feign.Feign;
 import feign.Param;
 import feign.RequestLine;
-import feign.jackson.JacksonDecoder;
-import feign.jackson.JacksonEncoder;
-import feign.okhttp.OkHttpClient;
-import feign.reactive.ReactorFeign;
 import feign.slf4j.Slf4jLogger;
-import org.gty.demo.config.properties.FeignLoggingProperties;
 import org.gty.demo.github.model.Contributor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import reactor.core.publisher.Mono;
-import reactor.core.scheduler.Scheduler;
 
 import javax.annotation.Nonnull;
 import java.util.Objects;
@@ -31,21 +25,11 @@ public interface ReactiveGithubService {
 
         @Nonnull
         @Bean
-        public ReactiveGithubService githubService(@Nonnull final FeignLoggingProperties properties,
-                                                   @Nonnull final ObjectMapper mapper,
-                                                   @Nonnull final Scheduler scheduler) {
-            Objects.requireNonNull(properties, "properties must not be null");
-            Objects.requireNonNull(mapper, "mapper must not be null");
-            Objects.requireNonNull(scheduler, "scheduler must not be null");
+        public ReactiveGithubService githubService(@Nonnull final Feign.Builder feign) {
+            Objects.requireNonNull(feign, "feign must not be null");
 
-            return ReactorFeign
-                .builder()
-                .scheduleOn(scheduler)
+            return feign
                 .logger(new Slf4jLogger(ReactiveGithubService.class))
-                .logLevel(properties.getLevel())
-                .encoder(new JacksonEncoder(mapper))
-                .decoder(new JacksonDecoder(mapper))
-                .client(new OkHttpClient())
                 .target(ReactiveGithubService.class, GITHUB_BASE_URL);
         }
     }
